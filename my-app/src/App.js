b0d1e7440c6c32ba3c5f36546255b9dd5304409f
Samantha <toxicwow21@gmail.com>
2020-12-03 19:28:50 -0500

    finished users.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import User from './User'
+import Input from './Input'
+import Followers from './Followers'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios'
+import { render } from '@testing-library/react'
+
+class App extends React.Component {
+    state = {
+        user: null,
+        query: '',
+        followers: [],
+        error: null
+    }
+
+    componentDidMount() {
+        const baseUrl = 
+
+    }
+
 }
+
+inputHandler = e => this.setState({})
+
+submitHandler = e => {
+
+}
+
+clickHandler = id => {
+
+}
+
+render() {
+    if (this.state.user) {
+        return (
+            <div className= 'App'>
+                <header className= 'App-header'>
+                    <Input error= {this.state.error} submit= {this.submitHandler} value= {this.state.query} change={this.inputHandler} />
+                    <User main={true} user= {this.state.user}/>
+                    <Followers click= {this.clickHandler} followers= {this.state.followers} />
+                </header>
+            </div>
+        )
+    } else if (this.state.error) {
+        return <h1>{this.state.error}</h1>
+    } else {
+        return <h1>Loading...</h1>
+    }
+}
+
+
 
 export default App;
